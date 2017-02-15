cd28e9cdd0d111f9af0cdbdb8edaec57540579d2
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2017-02-15 12:52:54 +0100

    add restaurant and review models and controllers

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem "bootstrap-sass"
 
 gem "simple_form"
 
+gem 'font-awesome-sass'
+
+gem 'autoprefixer-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
@@ -46,6 +50,7 @@ group :development, :test do
   gem "better_errors"
   gem "binding_of_caller"
 
+  gem 'rspec-rails'
 
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name: "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:  "french",
+  },
+  {
+    name:         "Le Dragon qui fume",
+    address:      "4 rue Blainville 75005 Paris",
+    category:  "chinese",
+  },
+  {
+    name:         "Hitachi",
+    address:      "route de Suresnes 75016 Paris",
+    category:  "japanese",
+  },
+  {
+    name:         "Chez Leon",
+    address:      "route de Versailles 75018 Paris",
+    category:  "belgian",
+  },
+  {
+    name:         "Le Petit bouchon",
+    address:      "route de Paris 33300 Bordeaux",
+    category:  "italian",
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
